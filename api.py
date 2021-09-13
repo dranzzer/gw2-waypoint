33d95705b4fadc705f7aa9afb6f1a484269a5b5c
dranzzer <89338308+dranzzer@users.noreply.github.com>
2021-09-13 14:07:32 +0530

    restructing for loop for better results

--- api.py
+++ api.py
@@ -14,14 +14,23 @@ wp_db = []
 #print(result_json[0]["regions"]["1"]["maps"]["26"]["points_of_interest"])
 
 #pulling from api
-waypoint_pull =result_json[0]["regions"]["1"]["maps"]["26"]["points_of_interest"]
-for k, v in waypoint_pull.items():
-    for a,s in v.items():
-        if s == "waypoint":
-            wp_db.append(v)
+#waypoint_pull =result_json[0]["regions"]["1"]["maps"]["26"]["points_of_interest"]
+#for k, v in waypoint_pull.items():
+#    for a,s in v.items():
+#        if s == "waypoint":
+#           wp_db.append(v)
+
+wp_pull =result_json[0]["regions"]["1"]["maps"]
+for map_number,map_data in wp_pull.items():
+    for name,min_level in map_data.items():
+        if name =="points_of_interest":
+            for x,c in min_level.items():
+                for q,w in c.items():
+                    print(q,w)
+                
 
 
-print(len(wp_db))
+#print(len(wp_db))
 
 
 #with open('output.json','w') as fs:
