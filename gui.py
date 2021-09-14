40355bafbc26e094eeccb0f65f48852ef8108e33
dranzzer <89338308+dranzzer@users.noreply.github.com>
2021-09-14 07:36:21 +0530

    merging gui with main.py

--- gui.py
+++ gui.py
@@ -14,8 +14,11 @@ win.title("ScrolledText Widget")
 # Title Label
 
   
-def reply(name):
-    print(name)
+def search(search):
+    for q1,q2 in test_list.items():
+        if search.lower() in q1.lower():
+            print("Key: " +q1)
+            print("Value: " +q2)
 
 
 
@@ -23,9 +26,9 @@ def reply(name):
 
 Label(win, text="search:").pack(side=TOP)
 ent = Entry(win)
-ent.bind("<Return>", (lambda event: reply(ent.get())))
+ent.bind("<Return>", (lambda event: search(ent.get())))
 ent.pack(side=TOP)
-btn = Button(win,text="Submit", command=(lambda: reply(ent.get())))
+btn = Button(win,text="Submit", command=(lambda: search(ent.get())))
 btn.pack(side=LEFT)
 
 
@@ -37,8 +40,7 @@ text_area = scrolledtext.ScrolledText(win,
                                       height = 10, 
                                       font = ("Times New Roman",
                                               15))
-test_list = ["apple", "banana", "cherry"]           
-
+test_list = {"Sam": "Blue", "Amy": "Red", "Sarah": "Yellow"}
 
 text_area.insert('end',test_list)
 text_area.pack()
--- main.py
+++ main.py
@@ -1,28 +1,51 @@
-#import tkinter as tk
-import pyperclip as pc
 
-#window = tk.Tk()
+# Python program demonstrating
+# ScrolledText widget in tkinter
+  
+import tkinter as tk
+from tkinter import ttk
+from tkinter import scrolledtext
+from tkinter import *
+  
+# Creating tkinter main window
+win = tk.Tk()
+win.title("ScrolledText Widget")
+  
+# Title Label
+
+  
+def search(search):
+    for q1,q2 in test_list.items():
+        if search.lower() in q1.lower():
+            print("Key: " +q1)
+            print("Value: " +q2)
 
 
 
-auric_basin_wp = {
-    "chak hollow waypoint" : "[&BEkIAAA=]",
-    "eastwatch waypoint" : "[&BGwIAAA=]",
-    "northwatch waypoint" : "[&BN0HAAA=]",
-    "southwatch waypoint" : "[&BAIIAAA=]",
-    "forgotten city  waypoint" : "[&BMYHAAA=]",
-    "wanderer's waypoint" : "[&BNYHAAA=]",
-    "westwatch waypoint" : "[&BAYIAAA=]"
-}
 
-search = input("Enter waypoint here")
 
-for wp_name,wp_code in auric_basin_wp.items():
-    if search in wp_name:
-        print(wp_name , wp_code)
-        pc.copy(wp_code)
-    
+Label(win, text="search:").pack(side=TOP)
+ent = Entry(win)
+ent.bind("<Return>", (lambda event: search(ent.get())))
+ent.pack(side=TOP)
+btn = Button(win,text="Submit", command=(lambda: search(ent.get())))
+btn.pack(side=LEFT)
 
-#test
-#window.mainloop()
+
+# Creating scrolled text 
+# area widget
+text_area = scrolledtext.ScrolledText(win, 
+                                      wrap = tk.WORD, 
+                                      width = 40, 
+                                      height = 10, 
+                                      font = ("Times New Roman",
+                                              15))
+test_list = {"Sam": "Blue", "Amy": "Red", "Sarah": "Yellow"}
+
+text_area.insert('end',test_list)
+text_area.pack()
+  
+# Placing cursor in the text area
+text_area.focus()
+win.mainloop()
 
