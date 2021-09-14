8e8d7c8f8e4bcea139a0d91770971761bbdb4611
dranzzer <89338308+dranzzer@users.noreply.github.com>
2021-09-14 07:45:39 +0530

    merged api.py with main.py and removed depreciated code

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-
-# Python program demonstrating
-# ScrolledText widget in tkinter
-  
 import tkinter as tk
 from tkinter import ttk
 from tkinter import scrolledtext
 from tkinter import *
+import requests
+import json
+result = requests.get("https://api.guildwars2.com/v2/continents/1/floors?ids=1")
+result.status_code
+result_json = result.json()
+wp_db = []
   
 # Creating tkinter main window
 win = tk.Tk()
@@ -44,8 +46,39 @@ test_list = {"Sam": "Blue", "Amy": "Red", "Sarah": "Yellow"}
 
 text_area.insert('end',test_list)
 text_area.pack()
-  
+
+
+              
+
+wp_pull = result_json[0]["regions"]
+for region_id,region in wp_pull.items():
+    for a,b in region.items():
+        if a =="maps":
+            for c,v in b.items():
+                for f,g in v.items():
+                    if f=="points_of_interest":
+                        for u,i in g.items():
+                            for q1,q2 in i.items():
+                                if q2=="waypoint":
+                                    wp_db.append(i)
+
+
+
+
 # Placing cursor in the text area
 text_area.focus()
 win.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
